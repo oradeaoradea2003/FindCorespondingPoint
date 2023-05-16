3d7f516104008f415f464e02ba96b43eff5ec509
Lucian Muresan <73871767+oradeaoradea2003@users.noreply.github.com>
2023-05-16 23:01:23 +0000

    com 3

---
+++ cg.c
@@ -0,0 +1,16 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+  
+
+int n=38;
+
+  main()
+  {
+
+printf("WELCOME LUCKY LUK\n    YOU THE THE BEST...THE ONE::: %d  \n ::" , n );
+printf("\n UN ZIMBET O FLOARE O RAZA DE SOARE    ");
+return 0;
+
+
+  }
--- g.cpp
+++ g.cpp
@@ -3,10 +3,12 @@
 
   using namespace std;
 
+int n=38;
+
   main()
   {
 
-cout << "WELCOME LUCKY LUK\n    YOU THE THE BEST...THE ONE";
+cout << "WELCOME LUCKY LUK\n    YOU THE THE BEST...THE ONE:::   \n ::  "  << n ;
 cout << "\n UN ZIMBET O FLOARE O RAZA DE SOARE    ";
 return 0;
 
